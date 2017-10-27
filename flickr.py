aef394d2aabc40ed32f73bfc071ea268eec7f86b
Adonais0 <chuyao@umich.edu>
2017-10-27 09:27:21 -0400

    general the func

--- flickr.py
+++ flickr.py
@@ -25,19 +25,12 @@ def params_unique_combination(baseurl, params_d, private_keys=["api_key"]):
             res.append("{}-{}".format(k, params_d[k]))
     return baseurl + "_".join(res)
 
-def search_flickr_by_tags(tags):
+def search_flickr(para_dict):
+    a = input("Please enter the key word you want to search(tags/photo_id):")
+    method = input("Enter the method you want to use:")
     if not FLICKR_API_KEY:
         raise Exception('Flickr API Key is missing!')
-
     baseurl = "https://api.flickr.com/services/rest/"
-    params_diction = {
-        "method": "flickr.photos.search",
-        "format": "json",
-        "api_key": FLICKR_API_KEY,
-        "tags": tags,
-        "per_page": 10,
-        "nojsoncallback": 1
-    }
 
     unique_ident = params_unique_combination(baseurl,params_diction)
     if unique_ident in CACHE_DICTION:
